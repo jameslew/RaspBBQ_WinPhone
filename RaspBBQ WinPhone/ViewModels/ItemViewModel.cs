c79416684316c00b1d044c11e3f50e5fd7807b96
jameslew <jameslew@live.com>
2014-02-17 21:56:20 -0800

    First Checkin,
    Just template UI plus a few controls and a view model to bind to.

---
+++ RaspBBQ WinPhone/ViewModels/ItemViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace RaspBBQ_WinPhone.ViewModels
+{
+    public class BBQStatusViewModel : INotifyPropertyChanged
+    {
+        private DateTime _startDateTime;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime StartDateTime
+        {
+            get
+            {
+                return _startDateTime;
+            }
+            set
+            {
+                if (value != _startDateTime)
+                {
+                    _startDateTime = value;
+                    NotifyPropertyChanged("StartDateTime");
+                }
+            }
+        }
+
+        private double _pitTemp;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public double PitTemp
+        {
+            get
+            {
+                return _pitTemp;
+            }
+            set
+            {
+                if (value != _pitTemp)
+                {
+                    _pitTemp = value;
+                    NotifyPropertyChanged("PitTemp");
+                }
+            }
+        }
+
+        private double _foodTemp;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public double FoodTemp
+        {
+            get
+            {
+                return _foodTemp;
+            }
+            set
+            {
+                if (value != _foodTemp)
+                {
+                    _foodTemp = value;
+                    NotifyPropertyChanged("FoodTemp");
+                }
+            }
+        }
+
+        private DateTime _lastUpdate;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime LastUpdate
+        {
+            get
+            {
+                return _lastUpdate;
+            }
+            set
+            {
+                if (value != _lastUpdate)
+                {
+                    _lastUpdate = value;
+                    NotifyPropertyChanged("LastUpdate");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
